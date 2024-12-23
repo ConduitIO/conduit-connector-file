02b18ea6e7fb83debaebc79b31b1ecc4a7d49bc6
Haris Osmanagic <haris@meroxa.io>
2024-12-23 18:48:38 +0100

    Generate connector.yaml

--- connector.go
+++ connector.go
@@ -12,12 +12,21 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:generate specgen
+
 package file
 
-import sdk "github.com/conduitio/conduit-connector-sdk"
+import (
+	_ "embed"
+
+	sdk "github.com/conduitio/conduit-connector-sdk"
+)
+
+//go:embed connector.yaml
+var specs string
 
 var Connector = sdk.Connector{
-	NewSpecification: Specification,
+	NewSpecification: sdk.YAMLSpecification(specs),
 	NewSource:        NewSource,
 	NewDestination:   NewDestination,
 }
